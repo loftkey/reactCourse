a793132de62e93e49aeb13f62dd4f4f384000aba
Andrew Price <005475141@coyote.csusb.edu>
2019-08-06 10:37:52 -0700

    Section 5 ep 68

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import './App.css';
 import Person from './Person/Person';
+import Radium from 'radium';
 
 class App extends Component {
   state = {
@@ -26,7 +27,7 @@ class App extends Component {
     const persons = [...this.state.persons];
     persons[personIndex] = person;
 
-    this.setState({ persons: persons})
+    this.setState({ persons: persons});
   }
 
   deletePersonHandler = (personIndex) => {
@@ -44,11 +45,16 @@ class App extends Component {
 
   render() {
     const style = {
-      backggroundColor: 'white',
+      backgroundColor: 'green',
+      color: 'white',
       font: 'inherit',
       border: '1px solid blue',
       padding: '8px',
-      cursor: 'pointer'
+      cursor: 'pointer',
+      ':hover': {
+        backgroundColor: 'lightgreen',
+        color : 'black'
+      }
     };
 
     let persons = null;
@@ -66,12 +72,27 @@ class App extends Component {
           })}
         </div>
       );
+
+      style.backgroundColor = 'red';
+      style[':hover'] = {
+        backgroundColor: 'salmon',
+        color : 'black'
+      }
     }
+
+    const classes = [];
+    if (this.state.persons.length <= 2) {
+      classes.push('red'); // classes = ['red']
+    }
+    if (this.state.persons.length <= 1) {
+      classes.push('bold'); //classes = ['red', 'bold]
+    }
+
 
     return (
       <div className="App">
         <h1>Hi, I'm a React App</h1>
-        <p>This is really working</p>
+        <p className={classes.join(' ')}>This is really working</p>
         <button 
         style={style}
         onClick={this.togglePersonsHandler}
@@ -82,4 +103,4 @@ class App extends Component {
   }
 }
 
-export default App;
+export default Radium(App);
